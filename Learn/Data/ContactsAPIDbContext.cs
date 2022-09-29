aab61745568d601ac11d528b85bed3fb727f2d31
Syed Hammad (starCoder) <syedhammad626@gmail.com>
2022-09-29 22:08:19 +0530

    Learn

---
+++ Learn/Data/ContactsAPIDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Learn.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Learn.Data
+{
+    public class ContactsAPIDbContext : DbContext
+    {
+        public ContactsAPIDbContext(DbContextOptions<ContactsAPIDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Contact> Contacts { get; set; }
+    }
+}
